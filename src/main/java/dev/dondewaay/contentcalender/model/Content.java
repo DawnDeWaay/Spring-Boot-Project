083199f3d13f945065d0322d80c78b05b337eb2a
DonDeWaay <dondewaay@gmail.com>
2024-04-03 20:12:02 -0500

    Docker file added

--- src/main/java/dev/dondewaay/contentcalender/model/Content.java
+++ src/main/java/dev/dondewaay/contentcalender/model/Content.java
@@ -1,9 +1,12 @@
 package dev.dondewaay.contentcalender.model;
 
+import jakarta.validation.constraints.NotEmpty;
+
 import java.time.LocalDateTime;
 
 public record Content(
         Integer id,
+        @NotEmpty
         String title,
         String desc,
         Status status,
